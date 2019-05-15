a550ec14e655067aa21e2fa76f973b3d67bf6d90
Brianna Kelsey <briannagkelsey@gmail.com>
2019-05-14 23:04:02 -0400

    connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+
+if (process.env.JAWSDB_URL) {
+   connection = mysql.createConnection(process.env.JAWSDB_URL);
+ } else {
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "@2TOOto101",
-  database: "burger_db",
-  socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+  database: "burger_db"
 });
+}
 
 // Make connection.
 connection.connect(function(err) {
